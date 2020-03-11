d6b6d144568bec17c2df6d61cee654bbfe60bc3d
aminulsarkerbd <55542703+aminulsarkerbd@users.noreply.github.com>
2020-03-11 18:36:33 +0600

    first jsx commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done! <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <h1>
+          My First React Experience!
+        </h1>
+
+        <Person name='Apple' description='Green Apple'></Person>
+        <Person name='Coconut' description='Green Coconut'></Person>
+        <Person name='Mango' description='Yellow Mango'></Person>
+
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  return (<div style={{ border: '3px solid #fcd454', margin: '5px', padding: '5px', width: '500px', borderRadius: '5px', backgroundColor: '#fff', color: '#000' }}>
+    <h1>Name: {props.name}</h1>
+    <p>Description: {props.description}</p>
+  </div>)
 }
 
 export default App;
