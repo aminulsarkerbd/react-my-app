44762e31265f113223ff710a6415844ccf2b2857
aminulsarkerbd <55542703+aminulsarkerbd@users.noreply.github.com>
2020-03-11 20:54:41 +0600

    props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'Smart Watch', price: '$49' },
+    { name: 'Smart Phone', price: '$599' },
+    { name: 'Ear Pod', price: '$99' }
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +19,9 @@ function App() {
         <Person name='Apple' description='Green Apple'></Person>
         <Person name='Coconut' description='Green Coconut'></Person>
         <Person name='Mango' description='Yellow Mango'></Person>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
 
       </header>
     </div>
@@ -26,5 +34,24 @@ function Person(props) {
     <p>Description: {props.description}</p>
   </div>)
 }
-
+function Product(props) {
+  const productStyle = {
+    width: '250px',
+    height: '250px',
+    float: 'left',
+    margin: '5px',
+    padding: '5px',
+    backgroundColor: '#eee',
+    color: '#000',
+    borderRadius: '5px'
+  }
+  const { name, price } = props.product;
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h3>{price}</h3>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 export default App;
