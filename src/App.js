b8d5a68f9c6a26d1ba0cbe470b733a2c218a9159
aminulsarkerbd <55542703+aminulsarkerbd@users.noreply.github.com>
2020-03-12 10:32:27 +0600

    react core concept exposed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +6,8 @@ function App() {
   const products = [
     { name: 'Smart Watch', price: '$49' },
     { name: 'Smart Phone', price: '$599' },
-    { name: 'Ear Pod', price: '$99' }
+    { name: 'Ear Pod', price: '$99' },
+    { name: 'Tab', price: '$999' }
   ];
   return (
     <div className="App">
@@ -15,17 +16,58 @@ function App() {
         <h1>
           My First React Experience!
         </h1>
-
+        <Counter></Counter>
+        <Users></Users>
+        <ul>
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+        {
+          products.map(prd => <Product product={prd}></Product>)
+        }
         <Person name='Apple' description='Green Apple'></Person>
         <Person name='Coconut' description='Green Coconut'></Person>
         <Person name='Mango' description='Yellow Mango'></Person>
-        <Product product={products[0]}></Product>
+        {/*<Product product={products[0]}></Product>
         <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
+      <Product product={products[2]}></Product>*/}
 
       </header>
     </div>
   );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => { setCount(count + 1); };
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => { setCount(count + 1); }}>Increase</button>
+      <button onClick={() => { setCount(count - 1); }}>Decrease</button>
+    </div >
+  )
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  return (
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 function Person(props) {
